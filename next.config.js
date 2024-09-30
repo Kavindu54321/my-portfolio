a306dc056349594841e75df0ce68999343b36dc9
Kavindu54321 <you@example.com>
2024-09-30 12:43:48 -0700

    init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
+  // Set the output to 'export' for static exports
+  output: 'export',
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-
   },
+
   images: {
     remotePatterns: [
       {
@@ -19,7 +22,5 @@ module.exports = {
       },
     ],
   },
-}
-module.exports = {
-  distDir: 'build',
 };
+
